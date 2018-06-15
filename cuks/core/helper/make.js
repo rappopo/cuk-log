6651217c79afbc4c1e2c8bd9c75db6816c6020af
Ardhi Lukianto <ardhi@lukianto.com>
2018-06-16 06:29:30 +0700

    reg update

--- cuks/core/helper/make.js
+++ cuks/core/helper/make.js
@@ -1,17 +1,18 @@
 'use strict'
 
 module.exports = function(cuk) {
+  const { _, path, fs, helper } = cuk.lib
   const pkgId = 'log'
   const pkg = cuk.pkg[pkgId]
-  const winston = pkg.lib.winston
 
   return (options = {}) => {
+    const winston = pkg.lib.winston
     const dir = path.join(cuk.dir.data, 'log', options.pkgId),
       file = path.join(dir, options.name + '.log')
     const name = `${options.pkgId}:${options.name}`
     fs.ensureDirSync(dir)
     if (!_.has(winston.loggers.loggers, name)) {
-      let cfg = helper('core:merge')(pkg.cfg.default, _.omit(options, ['json']))
+      let cfg = helper('core:merge')(pkg.cfg.common.opts, _.omit(options, ['json']))
       cfg.file = file
       winston.loggers.add(name, {
         transports: [ new winston.transports.Rotate(cfg)]
